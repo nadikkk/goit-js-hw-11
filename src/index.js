98c3d2b76143d02710dc27401e3086734fd8c514
nadikkk <109106413+nadikkk@users.noreply.github.com>
2022-12-28 02:16:08 +0200

    do it

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".search-form");
+const btnButton = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
+let page = 1;
+let nameImg = '';
+
+btnButton.classList.add('visually-hidden');
+form.addEventListener('input', onNameImg);
+form.addEventListener('submit', onSubmitNameImg);
+btnButton.addEventListener('click', onLoadMore);
+
+function onNameImg(e) {
+	nameImg = e.target.value;
+return nameImg
+
+}
+
+function onSubmitNameImg(e) {
+	e.preventDefault();
+	page = 1;
+	fetchNameImg(nameImg, page)
+	.then(renderCardImg)
+	.catch(error=>console.log(error));
+	gallery.innerHTML='';
+	btnButton.classList.remove('visually-hidden');
+}
+function fetchNameImg(nameImg, page) {
+	return fetch(`https://pixabay.com/api?key=32401247-d831a8438a21bb86fb66fd7b1&q=${nameImg}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
+	.then(r => r.json())
+}
+function renderCardImg({hits, total}) {
+	if (total===0) {
+		Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+	}
+	const markup = hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads})=> {
+		return `<div class="photo-card">
+		<img src="${webformatURL}" alt="${tags}" loading="lazy" />
+		<div class="info">
+		  <p class="info-item">
+			 <b>Likes: </b>${likes}
+		  </p>
+		  <p class="info-item">
+			 <b>Views: </b>${views}
+		  </p>
+		  <p class="info-item">
+			 <b>Comments: </b>${comments}
+		  </p>
+		  <p class="info-item">
+			 <b>Downloads: </b>${downloads}
+		  </p>
+		</div>
+	 </div>`;
+	}).join("");
+
+	gallery.insertAdjacentHTML('beforeend', markup)
+}
+function onLoadMore(e) {
+	page+=1;
+	fetchNameImg(nameImg, page)
+	.then(r => {
+		renderCardImg(r);
+		if ((r.total/40)<40) {
+			btnButton.classList.add('visually-hidden');
+			Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+		}
+	})
+	.catch(error=>console.log(error));
+}
+
+
